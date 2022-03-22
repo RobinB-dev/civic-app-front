63beda1cb56078e496729512c5510627deb39fa2
Rob1 <robin.bourachot@hetic.net>
2022-03-22 23:32:34 +0100

    :zap: Reduce size

--- App.tsx
+++ App.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import Navigation from "./src/navigation";
 import { AuthProvider } from "./src/provider/AuthProvider";
 import { ThemeProvider } from "react-native-rapi-ui";
-import { LogBox } from "react-native";
+// import { InMemoryCache, ApolloProvider } from "@apollo/client";
+// import ApolloClient from "apollo-boost";
+
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 // Initialize Apollo Client
@@ -17,13 +19,6 @@ export default function App() {
     require("./assets/images/register.png"),
     require("./assets/images/forget.png"),
   ];
-
-  // Ignore firebase v9 AsyncStorage warning
-  React.useEffect(() => {
-    LogBox.ignoreLogs([
-      "AsyncStorage has been extracted from react-native core and will be removed in a future release. It can now be installed and imported from '@react-native-async-storage/async-storage' instead of 'react-native'. See https://github.com/react-native-async-storage/async-storage",
-    ]);
-  }, []);
 
   return (
     <ApolloProvider client={client}>
